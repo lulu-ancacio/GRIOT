26cfc3626a2c763331adbac0c57f44168bd2b3ea
GomezaS2 <mariaeduardagds2007@gmail.com>
2026-05-19 23:51:10 -0300

    Refatora logout.php e teste_bd.php: remove nova linha no final do arquivo e ajusta a inclusão do supabase.php, além de melhorar a estrutura do HTML em teste_bd.php

--- conexao/teste_bd.php
+++ conexao/teste_bd.php
@@ -1,12 +1,20 @@
 <?php
-require 'supabase.php';
+require_once 'supabase.php';
 
 $itens = supabaseRequest("pinturas?select=*");
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="icon" href="meanStyle/assets/images/FavIcon_SF.png">
+    <meta name="description" content="Painel Administrativo ">
+    <title>GRIOT-Teste</title>
+</head>
 <body>
+
 
 <h1>Acervo</h1>
 
@@ -14,7 +22,7 @@ $itens = supabaseRequest("pinturas?select=*");
     <div>
         <h2><?php echo $item['titulo']; ?></h2>
         <p><?php echo $item['autor']; ?></p>
-        <img src="<?php echo $item['url'];?>">
+        <img src="<?php echo $item['url'];?>" alt="<?php echo $item['titulo']; ?>" width="200">
     </div>
 <?php endforeach; ?>
 
