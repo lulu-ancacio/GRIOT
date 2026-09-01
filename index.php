6945d21e1f9a0ce0e00ecf25572afdd4c678d0bd
GomezaS2 <mariaeduardagds2007@gmail.com>
2026-09-01 02:06:32 -0300

    feat: adicionar script do Leaflet para funcionalidades de mapa

--- index.php
+++ index.php
@@ -422,6 +422,9 @@ session_start();
     const vw = new window.VLibras.Widget('https://vlibras.gov.br/app');
   </script>
 
+  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin="anonymous"></script>
+
   <script>
     const map = L.map('mapa', {
       zoomControl: false,
